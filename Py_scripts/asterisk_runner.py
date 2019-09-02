621f5de9bdaa9de32bccff1e1ee35169895809e6
Jeremy Howe <Jeremy.Howe01@gmail.com>
2019-09-02 14:55:10 -0400

    updated tutorial. added utility scripts to be used for convenience, incluing console clear functionality

--- Py_scripts/asterisk_runner.py
+++ Py_scripts/asterisk_runner.py
@@ -1,10 +1,13 @@
 import os
+import utl
 import tutorial
+
+# Detect if the player has run the game before. 
+# If not, create a save file folder and run the tutorial
 
 if os.path.exists("../Save_Files"):
     # Returning player! Go to planet selection and game
     print("Welcome back!")
-    # os.rmdir("../Save_Files") -- Used for testing purposes
 else:
     try:
         os.mkdir("../Save_Files")
@@ -12,4 +15,4 @@ else:
         print("File initialization failed. Please reinstall the game ( sorry I'm bad programmer :< )")
     else:
         # NEW PLAYER! Run the intro and tutorial!
-        tutorial.runTutorial()
+        tutorial.run()
--- Py_scripts/tutorial.py
+++ Py_scripts/tutorial.py
@@ -1,4 +1,25 @@
+from utl import clear
 
+startMessage = """
+Welcome.
 
-def runTutorial():
-    print("Hello user! Welcome to a text adventure game!")
+You are floating in a small metal pod in deep space.
+Soon, you'll be landing on a planet.
+The planet is lush, alive, and dangerous.
+You are alone. Except for DALE.
+"""
+commandsMessage = """
+To navigate the planet, enter cardinal directions (NORTH, SOUTH, EAST, WEST) when prompted.
+To learn more about your status, enter STATUS
+To consult your Digital-Arm-Linked-Escort, enter DALE
+To explore an area, enter EXPLORE
+
+To review these commands, enter HELP
+"""
+
+def run():
+    print(startMessage)
+    input("<Enter to Continue>")
+    print(commandsMessage)
+    input("<Enter to Continue>")
+    clear()
