ce14351bf8b20a3e305ba775098303077641e56c
Jeremy Howe <Jeremy.Howe01@gmail.com>
2019-09-02 13:55:54 -0400

    successful directory check and creation

--- Py_scripts/asterisk_runner.py
+++ Py_scripts/asterisk_runner.py
@@ -1,2 +1,14 @@
+import os
 import os.path
 
+welcomeString = "Hello user! Welcome to a text adventure game!"
+
+if os.path.exists("../Save_Files"):
+    print("Welcome back!")
+else:
+    try:
+        os.mkdir("../Save_Files")
+    except OSError:
+        print("File initialization failed")
+    else:
+        print(welcomeString)
